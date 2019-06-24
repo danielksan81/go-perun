7a8e01d7544a4969692d0dfa67813e64e2b884ca
MariusVanDerWijden <m.vanderwijden@live.de>
2019-06-24 16:10:50 +0200

    :art: added Wallet() to Account interface

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -19,6 +19,9 @@ type Account interface {
 	// Address used by this account
 	Address() common.Address
 
+	// Wallet returns the wallet this account belongs to.
+	Wallet() Wallet
+
 	// Path returns an optional resource locator within a backend
 	Path() string
 
