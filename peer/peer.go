4010e8c4583048004dbdf67052ffc7bf7a91d12c
Steffen Rattay <steffen@perun.network>
2019-09-04 18:55:58 +0200

    :sparkles: [peer] Implemented the peer concept.
    [WIP]

---
+++ peer/peer.go
@@ -0,0 +1,174 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+// Package peer contains the peer connection related code.
+package peer // import "perun.network/go-perun/peer"
+
+import (
+	"sync"
+
+	"github.com/pkg/errors"
+
+	"perun.network/go-perun/wire/msg"
+)
+
+// Peer is a Perun peer. It contains the peer's identity, and also represents
+// a communications channel with that peer. Peers must not be created manually.
+// Peers are thread-safe.
+//
+// If a peer's connection fails, it is automatically repaired as soon as a new
+// connection with the peer is established. This process is hidden.
+type Peer struct {
+	PerunAddress Address // The peer's perun address.
+
+	conn     Conn
+	subs     subscriptions
+	registry *Registry
+
+	replacing sync.Mutex // Protects conn from replaceConn and Close.
+	sending   sync.Mutex // Blocks multiple Send calls.
+	recving   sync.Mutex // Blocks multiple Recv calls.
+
+	closed    chan struct{} // Indicates whether the peer is closed.
+	retrySend chan struct{} // Signals that Send calls can retry. Capacity 1.
+	retryRecv chan struct{} // Signals that Recv calls can retry. Capacity 1.
+}
+
+// Recv receives a single message from a peer.
+// If the transmission fails, blocks until the connection is repaired and
+// retries to receive the message. Fails if the peer is closed via Close().
+func (p *Peer) recv() msg.Msg {
+	// Only one message can be received at a time.
+	p.recving.Lock()
+	defer p.recving.Unlock()
+
+	// Repeatedly attempt to receive a message.
+	for {
+		if m, err := p.conn.Recv(); err == nil {
+			return m
+		} else {
+			select {
+			case <-p.closed:
+				// Fail when the peer is closed.
+				return nil
+			case <-p.retryRecv:
+				// Retry when the connection is repaired.
+				continue
+			}
+		}
+	}
+}
+
+// recvLoop continuously receives messages from a peer until it is closed.
+// Received messages are relayed via the peer's subscription system. This is
+// called by the registry when the peer is registered.
+func (p *Peer) recvLoop() {
+	for {
+		if m := p.recv(); m == nil {
+			return
+		} else {
+			// Broadcast the received message to all interested subscribers.
+			p.subs.put(m, p)
+		}
+	}
+}
+
+// Send sends a single message to a peer.
+// If the transmission fails, blocks until the connection is repaired and
+// retries to send the message. Fails if the peer is closed via Close().
+func (p *Peer) Send(m msg.Msg) error {
+	// Only one message can be sent at a time.
+	p.sending.Lock()
+	defer p.sending.Unlock()
+
+	// Repeatedly attempt to send the message.
+	for p.conn.Send(m) != nil {
+		select {
+		case <-p.closed:
+			// Fail when the peer is closed.
+			return errors.New("connection closed")
+		case <-p.retrySend:
+			// Retry when the connection is repaired.
+			continue
+		}
+	}
+
+	return nil
+}
+
+// replaceConn replaces a peer's connection, if the peer itself is not closed.
+// Returns whether the peer's connection has been replaced.
+func (p *Peer) replaceConn(conn Conn) bool {
+	// Replacing is not thread-safe.
+	p.replacing.Lock()
+	defer p.replacing.Unlock()
+
+	if p.isClosed() {
+		// Abort if the peer is closed.
+		return false
+	}
+
+	// Clear the retrySend and retryRecv channels.
+	_, _ = <-p.retrySend
+	_, _ = <-p.retryRecv
+
+	// Close the old connection to fail all send and receive calls.
+	p.conn.Close()
+	p.conn = conn
+
+	// Send the retry signal to both send and recv.
+	p.retrySend <- struct{}{}
+	p.retryRecv <- struct{}{}
+
+	return true
+}
+
+// isClosed checks whether the peer is marked as closed.
+// This is different from the peer's connection being closed, in that a closed
+// peer cannot have its connection restored and is marked for deletion.
+func (p *Peer) isClosed() bool {
+	select {
+	case <-p.closed:
+		return true
+	default:
+		return false
+	}
+}
+
+// Close closes a peer's connection and deletes it from the registry. If the
+// peer was already closed, results in an error. A closed peer is no longer
+// usable.
+func (p *Peer) Close() error {
+	// Must not interfere with ongoing replacement and closing attempts.
+	p.replacing.Lock()
+	defer p.replacing.Unlock()
+
+	if p.isClosed() {
+		return errors.New("already closed")
+	}
+
+	// Mark the peer as closed.
+	close(p.closed)
+	// Close the peer's connection.
+	p.conn.Close()
+	// Unregister the peer.
+	p.registry.delete(p)
+
+	return nil
+}
+
+// newPeer creates a new peer from a peer address and connection.
+func newPeer(addr Address, conn Conn, registry *Registry) *Peer {
+	return &Peer{
+		PerunAddress: addr,
+
+		conn:     conn,
+		subs:     makeSubscriptions(),
+		registry: registry,
+
+		closed:    make(chan struct{}),
+		retrySend: make(chan struct{}, 1),
+		retryRecv: make(chan struct{}, 1),
+	}
+}
---
+++ peer/registry.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2019 The Perun Authors. All rights reserved.
+// This file is part of go-perun. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
+package peer
+
+import (
+	"sync"
+
+	"perun.network/go-perun/log"
+)
+
+// Registry is a peer Registry.
+// It should not be used manually, but only internally by the client.
+type Registry struct {
+	mutex sync.RWMutex
+	peers []*Peer
+
+	subscribe func(*Peer) // Sets up peer subscriptions.
+}
+
+// NewRegistry creates a new registry.
+// The provided callback is used to set up new peer's subscriptions and it is
+// called before the peer starts receiving messages.
+func NewRegistry(subscribe func(*Peer)) *Registry {
+	return &Registry{
+		subscribe: subscribe,
+	}
+}
+
+// find looks up a peer via its Perun address.
+// If found, returns the peer and its index, otherwise returns a nil peer.
+func (r *Registry) find(addr Address) (*Peer, int) {
+	for i, peer := range r.peers {
+		if peer.PerunAddress.Equals(addr) {
+			return peer, i
+		}
+	}
+
+	return nil, 0
+}
+
+// Find looks up the peer via its perun address.
+func (r *Registry) Find(addr Address) *Peer {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	p, _ := r.find(addr)
+	return p
+}
+
+// Register registers a peer in the registry.
+// If a peer with the same perun address already existed, replaces that peer's
+// connection with the new peer's connection, but keeps the old peer object.
+// Otherwise, enters the new peer into the registry.
+func (r *Registry) Register(addr Address, conn Conn) (peer *Peer) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if peer, _ = r.find(addr); peer == nil {
+		// Create and register a new peer.
+		peer = newPeer(addr, conn, r)
+		r.peers = append(r.peers, peer)
+		// Setup the peer's subscriptions.
+		r.subscribe(peer)
+		// Start receiving messages.
+		go peer.recvLoop()
+	} else {
+		peer.replaceConn(conn)
+	}
+
+	return
+}
+
+// delete deletes a peer from the registry.
+// If the peer does not exist in the registry, panics. Does not close the peer.
+func (r *Registry) delete(peer *Peer) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	for i, p := range r.peers {
+		if p == peer {
+			// Delete the i-th entry.
+			r.peers[i] = r.peers[len(r.peers)-1]
+			r.peers = r.peers[:len(r.peers)-1]
+			return
+		}
+	}
+
+	log.Panic("tried to delete non-existent peer!")
+}
