b606f2baca6c77c61b38a711167cbb6f788719d5
Steffen Rattay <steffen@perun.network>
2019-09-16 17:10:01 +0200

    :bug: [peer] Fixed freeze in peer.repair().
    Fixed a case in peer.repair() where a freshly dialed
    connection was left to rot, which caused the connection to
    hang.
    Changed the receiver and subscription interfaces to be more
    usable. Receiver can now detect whether it is subscribed to
    anything, and can be used to abort waiting for messages in
    case it is not subscribed to any peers.
    
    Added test for Peer.Close().

--- peer/peer_test.go
+++ peer/peer_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 
 	"perun.network/go-perun/backend/sim"
 	"perun.network/go-perun/wire/msg"
@@ -34,10 +35,10 @@ func MakeSetup() *Setup {
 		alice: MakeClient(a, rng, setup),
 		bob:   MakeClient(b, rng, setup),
 	}
-
 	return setup
 }
 
+// Dial simulates creating a connection to a peer.
 func (s *Setup) Dial(addr Address, abort <-chan struct{}) (Conn, error) {
 	select {
 	case <-s.closed:
@@ -48,12 +49,12 @@ func (s *Setup) Dial(addr Address, abort <-chan struct{}) (Conn, error) {
 	// a: Alice's end, b: Bob's end.
 	a, b := newPipeConnPair()
 
-	if addr.Equals(s.alice.Peer.PerunAddress) { // Dialing Bob?
-		s.bob.Registry.Register(addr, a) // Bob accepts connection.
-		return b, nil
-	} else if addr.Equals(s.bob.Peer.PerunAddress) { // Dialing Alice?
-		s.alice.Registry.Register(addr, b) // Alice accepts connection.
+	if addr.Equals(s.alice.partner.PerunAddress) { // Dialing Bob?
+		s.bob.Registry.Register(s.bob.partner.PerunAddress, b) // Bob accepts connection.
 		return a, nil
+	} else if addr.Equals(s.bob.partner.PerunAddress) { // Dialing Alice?
+		s.alice.Registry.Register(s.alice.partner.PerunAddress, a) // Alice accepts connection.
+		return b, nil
 	} else {
 		return nil, errors.New("unknown peer")
 	}
@@ -73,7 +74,7 @@ func (s *Setup) Close() error {
 // Client is a simulated client in the test setup.
 // All of the client's incoming messages can be read from its receiver.
 type Client struct {
-	*Peer
+	partner *Peer
 	*Registry
 	*Receiver
 }
@@ -82,13 +83,13 @@ type Client struct {
 func MakeClient(conn Conn, rng io.Reader, dialer Dialer) *Client {
 	var receiver = NewReceiver()
 	var registry = NewRegistry(func(p *Peer) {
-		receiver.Subscribe(p, msg.Control)
-		receiver.Subscribe(p, msg.Peer)
-		receiver.Subscribe(p, msg.Channel)
+		_ = receiver.Subscribe(p, msg.Control)
+		_ = receiver.Subscribe(p, msg.Peer)
+		_ = receiver.Subscribe(p, msg.Channel)
 	}, dialer)
 
 	return &Client{
-		Peer:     registry.Register(sim.NewRandomAccount(rng).Address(), conn),
+		partner:  registry.Register(sim.NewRandomAccount(rng).Address(), conn),
 		Registry: registry,
 		Receiver: receiver,
 	}
@@ -101,15 +102,13 @@ func TestConnectionRepair(t *testing.T) {
 	// Create a setup with two connected nodes.
 	setup := MakeSetup()
 	// Close both connections.
-	setup.alice.Peer.conn.Close()
-	setup.bob.Peer.conn.Close()
+	setup.alice.partner.conn.Close()
+	setup.bob.partner.conn.Close()
 
 	done := make(chan struct{})
 	// Send the message in the background.
 	go func() {
-		if nil != setup.alice.Peer.Send(msg.NewPingMsg(), nil) {
-			t.Error("Failed to send")
-		}
+		assert.Nil(t, setup.alice.partner.Send(msg.NewPingMsg(), nil), "failed to send")
 		close(done)
 	}()
 
@@ -119,4 +118,41 @@ func TestConnectionRepair(t *testing.T) {
 	}
 	// Ensure the sender is done, so that if it fails, the error is not lost.
 	<-done
+}
+
+// TestPeer_Close tests that closing a peer will make the peer object unusable,
+// and that the remote end will try to re-establish the connection.
+func TestPeer_Close(t *testing.T) {
+	setup := MakeSetup()
+	// Remember bob's address for later, we will need it for a registry lookup.
+	bobAddress := setup.alice.partner.PerunAddress
+	// The lookup needs to work because the test relies on it.
+	assert.Equal(t, setup.alice.partner, setup.alice.Registry.Find(bobAddress))
+	setup.alice.partner.Close() // Close Alice's connection to Bob.
+	// Sending over closed peers (not connections) must fail.
+	assert.NotNil(t, setup.alice.partner.Send(msg.NewPingMsg(), nil), "sending to bob must fail")
+	// Sending from the other side must succeed, as the remote will repair the
+	// peer connection.
+	assert.Nil(t, setup.bob.partner.Send(msg.NewPingMsg(), nil), "sending to alice must succeed (new socket)")
+	// The receiver is subscribed automatically because it is registered in the
+	// registry (see MakeClient()).
+	assert.NotNil(t, (<-setup.alice.Receiver.Next()).Msg, "new alice must receive")
+	// There must only be one peer in the registry (the closed peer deleted itself).
+	assert.Equal(t, 1, len(setup.alice.Registry.peers))
+
+	// Retrieve the new peer connection to bob and check that it is a new
+	// instance (different address) than the old, closed peer.
+	aliceNewPartner := setup.alice.Registry.Find(bobAddress)
+	// The new peer must not be nil.
+	assert.NotNil(t, aliceNewPartner)
+	// The new peer must not be the old, closed peer.
+	assert.True(t,
+		setup.alice.partner != aliceNewPartner,
+		"Alice needs to have been replaced")
+
+	// Check that the new peer can communicate in both directions.
+	assert.Nil(t, setup.bob.partner.Send(msg.NewPongMsg(), nil), "Sending to new Alice")
+	assert.Nil(t, aliceNewPartner.Send(msg.NewPingMsg(), nil), "new alice must send to bob")
+	assert.NotNil(t, (<-setup.alice.Receiver.Next()).Msg, "new alice must receive")
+	assert.NotNil(t, (<-setup.bob.Receiver.Next()).Msg, "bob must receive")
 }
